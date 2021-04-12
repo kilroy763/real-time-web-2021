2dda1952439eef6ccc4a4d0728615dd61ce610b6
kilroy763 <itzmaxkiljan@hotmail.com>
2021-04-12 15:28:14 +0200

    first code

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const app = express();
-var bodyParser = require('body-parser')
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
 const request = require('request');
-
 const path = require('path');
-
 const port = 5000;
-
 const render = require('./modules/render.js');
-
-
 
 // link de templating engine aan express
 app.set('view engine', 'ejs');
@@ -23,15 +16,11 @@ app.set('views', 'views');
 // Express laten weten dat er gebruik wordt gemaakt van een statisch folder
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use(bodyParser.urlencoded({ extended: true}))
-
-app.use(bodyParser.json())
-
 // Als de route "/" is dan moet de site geredirect worden naar "/breakingbad"
 app.get('/', redirect)
 
 function redirect(req, res) {
-	res.redirect('/home');
+	res.redirect('/test');
 }
 
 app.get('/home', render.home)
@@ -39,30 +28,28 @@ app.get('/home', render.home)
 app.get('/test', render.test)
 
 
-app.post('/test', render.test, function (req, res) {
-    var check= document.getElementById("check").value == "text_value";
-
-    //check will be true or false
-    if (check){
-        console.log('true')
-    } //do something if true}
-    if(!check){
-        console.log('false')
-    }//do something if false}
-} 
-)
-
-// app.get('/actor', render.actor);
-
-// app.get('/movie', render.movie);
-
-// app.get('/show', render.show);
+app.post('/test', render.test)
 
 
+const users = {}
 
 io.on('connection', (socket) => {
-    console.log('a user has connected')
-    console.log(socket.id)
+    console.log(socket.id + 'is geconnect')
+
+    socket.on("new-user", user => {
+        users[socket.id] = user
+        socket.broadcast.emit('user-connected', user)
+    })
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id] 
+    })
+
+    socket.on('send-chat-message', message => {
+        console.log(message)
+        socket.broadcast.emit('chat-message', {message: message, user: users[socket.id]})
+    })
 
     socket.on('disconnect', () => {
         console.log('user disconnected')
@@ -71,7 +58,6 @@ io.on('connection', (socket) => {
 
 
 // server set up
-
 http.listen(process.env.PORT || port, function (){
     console.log(`Application started on port: ${port}`);
 	console.log(`Application is visible on: http://localhost:${port}`);
--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,50 @@
-console.log('connected')
+console.log('script connected')
 
-let socket = io()
+const socket = io()
+const user = prompt('Wat is je gebruikersnaam?')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const answerInput = document.getElementById('answer-input')
 
-if (socket) {
-    console.log('socket connected to client')
+messageForm.addEventListener('submit', e =>{
+    e.preventDefault()
+    const message = answerInput.value
+
+    if ( message == '2019'){
+        var checkMSG = ". Hij heeft het goed!"
+    } else{
+        var checkMSG = ". Hij heeft het  fout!"
+    }
+
+    appendMessage(`Jij: ${message} ${checkMSG}`)
+    socket.emit('send-chat-message', message)
+    answerInput.value = ''
+})
+
+function appendMessage(message){
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
 }
+
+socket.emit('new-user', user)
+
+socket.on('connect', () => {console.log(socket.id + 'user connected')});
+
+socket.on('chat-message', data => {
+    console.log(data)
+    appendMessage(`${data.user}: ${data.message} `)
+})
+
+socket.on('user-connected', user => {
+    appendMessage(`${user} is erbij`)
+})
+
+socket.on('user-disconnected', user => {
+    appendMessage(`${user} is weg`)
+})
+
+
+
+
 
