66f795bfe35f98db3c4fc216645791813563cafc
kilroy763 <itzmaxkiljan@hotmail.com>
2021-04-20 17:10:55 +0200

    added some css

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const port = 5000;
 const render = require('./modules/render.js');
 const fetcher = require('./modules/fetch.js');
 const bodyParser = require('body-parser');
-const { json } = require('body-parser');
+const {
+    json
+} = require('body-parser');
 const fetch = require('node-fetch')
 
 // link de templating engine aan express
@@ -20,12 +22,14 @@ app.set('views', 'views');
 // Express laten weten dat er gebruik wordt gemaakt van een statisch folder
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.get('/', redirect)
 
 function redirect(req, res) {
-	res.redirect('/home');
+    res.redirect('/home');
 }
 
 app.get('/home', render.home)
@@ -61,26 +65,29 @@ io.on('connection', (socket) => {
 
 
     socket.on("new-user", user => {
-        
-            let roomData = []
-            user = {user: user, score: 0 }
-            users[socket.id] = user
-        
-            for (const [key, value] of Object.entries(users)) {
-                    roomData.push(users[key])
-            }
 
-            console.log(JSON.stringify(roomData) + ' dit is de roomdata')
-        
- 
+        let roomData = []
+        user = {
+            user: user,
+            score: 0
+        }
+        users[socket.id] = user
+
+        for (const [key, value] of Object.entries(users)) {
+            roomData.push(users[key])
+        }
+
+        console.log(JSON.stringify(roomData) + ' dit is de roomdata')
 
 
-        
-       
+
+
+
+
         // userID = socket.id
         // user = {user: user, score: 0, userID: userID}
         // users['allUsers'] = user
-    
+
         io.emit('user-connected', roomData)
         console.log(JSON.stringify(users) + ' dit is user array')
         console.log(JSON.stringify(user) + ' dit zijn de users')
@@ -88,10 +95,10 @@ io.on('connection', (socket) => {
 
     socket.on("disconnect", () => {
         let roomData = []
-        delete users[socket.id] 
+        delete users[socket.id]
         for (const [key, value] of Object.entries(users)) {
             roomData.push(users[key])
-    }
+        }
         console.log(JSON.stringify(users) + ' dit zijn de users na disconnect')
         socket.broadcast.emit('user-disconnected', roomData)
     })
@@ -99,60 +106,66 @@ io.on('connection', (socket) => {
     socket.on('send-chat-message', message => {
         console.log(message + ' dit is message')
 
-        socket.broadcast.emit('chat-message', {message: message, user: users[socket.id]})
+        socket.broadcast.emit('chat-message', {
+            message: message,
+            user: users[socket.id]
+        })
     })
 
     socket.on('point', data => {
 
         let roomData = []
 
-    
+
         let userId = socket.id
 
         let score = Number(data.score)
         let newScore = score + 1
         users[userId].score = newScore
-        
+
 
         console.log(users[userId].score + ' scoreee')
 
         for (const [key, value] of Object.entries(users)) {
             roomData.push(users[key])
-            }
+        }
         // console.log(JSON.stringify(roomData) + ' hier is de users')
 
-        
-    io.emit('point', roomData)
+
+        io.emit('point', roomData)
     })
-    
-    socket.on('correct-answer', async message =>{
-    // fetch
-    var max = '2021'
-    var min = '1990'
-    
-    var season = Math.floor(Math.random() * (+max - +min)) + +min;
-    
-    const URLSeasonQ = `http://ergast.com/api/f1/${season}/driverStandings.json`;
-    
-    async function fetchData(data){
-        const fetch_response = await fetch(URLSeasonQ)
-        const json = await fetch_response.json();
-        return json
-    }
+
+    socket.on('correct-answer', async message => {
+        // fetch
+        var max = '2021'
+        var min = '1990'
+
+        var season = Math.floor(Math.random() * (+max - +min)) + +min;
+
+        const URLSeasonQ = `http://ergast.com/api/f1/${season}/driverStandings.json`;
+
+        async function fetchData(data) {
+            const fetch_response = await fetch(URLSeasonQ)
+            const json = await fetch_response.json();
+            return json
+        }
 
 
-    var fetchTest = await fetchData()
-    io.emit('correct-answer-function', {message, fetchTest})
+        var fetchTest = await fetchData()
+        io.emit('correct-answer-function', {
+            message,
+            fetchTest
+        })
     })
 
 
     socket.on('check-answer', checkMSG => {
         console.log(checkMSG + ' checkmsg')
 
-        if (checkMSG == 'Dit is goed!'){
-           
+        if (checkMSG == 'Dit is goed!') {
+
             console.log('dit is goedd')
-        } else{
+        } else {
             console.log('dit is fouttt')
         }
 
@@ -163,7 +176,7 @@ io.on('connection', (socket) => {
 
 
 // server set up
-http.listen(process.env.PORT || port, function (){
+http.listen(process.env.PORT || port, function () {
     console.log(`Application started on port: ${port}`);
-	console.log(`Application is visible on: http://localhost:${port}`);
-  });
+    console.log(`Application is visible on: http://localhost:${port}`);
+});
--- public/js/script.js
+++ public/js/script.js
@@ -7,26 +7,21 @@ const messageForm = document.getElementById('send-container')
 const answerInput = document.getElementById('answer-input')
 const playerContainer2 = document.getElementById('playerContainer')
 
-
-messageForm.addEventListener('submit', e =>{
+messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message = answerInput.value
     const answer = document.getElementById('antwoord').innerHTML
 
-    if ( message === answer){
+    if (message === answer) {
         var checkMSG = "Dit is goed!"
         addPoint()
         socket.emit('correct-answer', message)
-    } else{
+    } else {
         var checkMSG = "Dit is fout!"
         socket.emit('false-answer', message)
     }
 
     messageContainer.scrollTop = messageContainer.scrollHeight
- 
-    console.log(message + ' check antwoord')
-    console.log(answer + ' check msg')
-
 
     appendMessage(`Jij: ${message}`)
     appendMessage(` ${checkMSG}`)
@@ -35,7 +30,7 @@ messageForm.addEventListener('submit', e =>{
     answerInput.value = ''
 })
 
-function appendMessage(message){
+function appendMessage(message) {
     const messageElement = document.createElement('div')
     messageElement.innerText = message
     messageContainer.append(messageElement)
@@ -43,138 +38,74 @@ function appendMessage(message){
 
 
 function updateList(data) {
-
     playerContainer2.innerHTML = ''
     data.forEach(obj => {
-
-        
-        let me 
-        if(obj.user === user){
+        let me
+        if (obj.user === user) {
             me = 'thisUser'
         } else {
             me = 'user'
         }
-
         playerContainer2.innerHTML += '<div class=' + me + '><p class="username">' + obj.user + '</p><p class="scores">' + obj.score + '</p></div>'
     })
 }
 
-function addPoint(){
+function addPoint() {
     let score = document.querySelector('.thisUser .scores').innerHTML
-
-    console.log(score + ' dit is de hduige score')
-
-    socket.emit('point', {user: user, score: score})
+    socket.emit('point', {
+        user: user,
+        score: score
+    })
 }
-
-
-
-// updates userlist, so scores and users
-
-
 
 socket.emit('new-user', user)
 
-socket.on('connect', () => {console.log(socket.id + 'user connected')});
+socket.on('connect', () => {
+    console.log(socket.id + 'user connected')
+});
 
 
-socket.on('point', data =>{
-    messageContainer.innerHTML += '<p>' + data.user + ' heeft het goed!</p>'
-
+socket.on('point', data => {
     let newScore = document.querySelector('.thisUser .scores')
-    // newScore.innerHTML = data.newScore
     updateList(data)
-    console.log(newScore.innerHTML)
-    console.log( JSON.stringify(data) + ' punt data')
 })
 
 socket.on('chat-message', data2 => {
-    console.log(JSON.stringify(data2) + 'dit is data 2')
     appendMessage(`${data2.user.user}: ${data2.message}  `)
 })
 
-socket.on('answer', checkMSG =>{
+socket.on('answer', checkMSG => {
     appendMessage(` ${checkMSG}`)
 })
 
-socket.on('correct-answer-function', correct =>{
-    console.log(correct)
-
+socket.on('correct-answer-function', correct => {
     var answer = document.getElementById('antwoord')
     answer.innerHTML = ''
     answer.innerHTML = `${correct.fetchTest.MRData.StandingsTable.season}`
-
     var tableRow = document.getElementById('tableBody')
     tableRow.innerHTML = '';
-
- 
-    // var score = document.getElementById('scores').innerHTML 
-    // console.log(score + ' dit is score ')
-    // let scores = Number(score)
-    // console.log(scores)
-    //     let newScore = scores + 1
-    //     document.getElementById('scores').innerHTML = newScore
-
-    // var score = document.getElementById('scores').value
-  
-
-
-
-    // var score = parseInt(document.getElementById('scores').value, 10);
-    // score = isNaN(score) ? 0 : score;
-    // +score++;
- 
-    // console.log(typeof(score) + ' scoreee')
-
-    
-
     correct.fetchTest.MRData.StandingsTable.StandingsLists[0].DriverStandings.forEach(standing => {
-    var table = document.getElementById('tableBody')
-    var row = table.insertRow(-1);
-    var position = row.insertCell(-1);
-    var driver = row.insertCell(1);
-    var wins = row.insertCell(2);
-    var points = row.insertCell(3);
-    var team = row.insertCell(4);
-    position.innerHTML = standing.position; 
-    driver.innerHTML = `${standing.Driver.givenName} ${standing.Driver.familyName}`;
-    wins.innerHTML = standing.wins
-    points.innerHTML = standing.points
-    team.innerHTML = standing.Constructors[0].name
+        var table = document.getElementById('tableBody')
+        var row = table.insertRow(-1);
+        var position = row.insertCell(-1);
+        var driver = row.insertCell(1);
+        var wins = row.insertCell(2);
+        var points = row.insertCell(3);
+        var team = row.insertCell(4);
+        position.innerHTML = standing.position;
+        driver.innerHTML = `${standing.Driver.givenName} ${standing.Driver.familyName}`;
+        wins.innerHTML = standing.wins
+        points.innerHTML = standing.points
+        team.innerHTML = standing.Constructors[0].name
     })
- 
+
 })
 
-
-
-
-
 socket.on('user-connected', userData => {
-    
     console.log(JSON.stringify(userData) + ' dit is de userdata')
-
     updateList(userData)
-    // userData.forEach(user => {
-    //     appendMessage(`${user.user} is erbij`)
-    //     appendPlayer( `${user.user} `)
-    //     appendScore(`${user.score}`)
-    // })
-
-  
-    // console.log(JSON.stringify(userData) + ' dit is de userdata')
-    // console.log(userData)
-
-    // appendPlayer( `${userData.users.allUsers.user} `)
-    // appendScore(`${userData.users.allUsers.score}`)
 })
 
 socket.on('user-disconnected', user => {
-    
     updateList(user)
 })
-
-
-
-
-
-
